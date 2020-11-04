0b3d83334f281a68fb3d1badcd7f7c6cb00546d3
Nazim Boudeffa <boudeffa@gmail.com>
2020-11-04 16:24:18 +0100

    Added parents to db

--- family-truffaut.js
+++ family-truffaut.js
@@ -6,6 +6,8 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('http://chaudoudoux.ml/families/add');
     await driver.findElement(By.name('input-family-code')).sendKeys('M00003');
     await driver.findElement(By.name('input-allocation-number')).sendKeys('123456789');
+    await driver.findElement(By.name('input-firstname-parent-father')).sendKeys('François');
+    await driver.findElement(By.name('input-lastname-parent-father')).sendKeys('Truffaut');
     await driver.findElement(By.name('btn-validate')).click();
     await driver.wait(until.titleIs('added'), 1000);
   } finally {
