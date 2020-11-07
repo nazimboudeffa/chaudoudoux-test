b7f0093aa051850fb5f57f103bb541e99b38d7a2
Nazim Boudeffa <boudeffa@gmail.com>
2020-11-07 01:30:47 +0100

    addchild works

--- family-truffaut.js
+++ family-truffaut.js
@@ -8,6 +8,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.findElement(By.name('input-allocation-number')).sendKeys('123456789');
     await driver.findElement(By.name('input-firstname-parent-father')).sendKeys('François');
     await driver.findElement(By.name('input-lastname-parent-father')).sendKeys('Truffaut');
+    await driver.findElement(By.id('add-child')).click();
+    await driver.findElement(By.name('input-lastname-child_1')).sendKeys('Truffaut');
+    await driver.findElement(By.name('input-firstname-child_1')).sendKeys('Éva');
     await driver.findElement(By.name('btn-validate')).click();
     await driver.wait(until.titleIs('added'), 1000);
   } finally {
