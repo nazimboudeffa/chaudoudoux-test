226c9a6c79b6c3ea570268f48e5cd7324edd6259
Nazim Boudeffa <boudeffa@gmail.com>
2020-11-18 21:57:23 +0100

    Samantha ofc

---
+++ candidate-samantha-mitchelli.js
@@ -0,0 +1,14 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
+  try {
+    await driver.get('http://chaudoudoux.ml/candidates/add');
+    await driver.findElement(By.name('input-name')).sendKeys('Samantha');
+    await driver.findElement(By.name('input-firstname')).sendKeys('Mitchelli');
+    await driver.findElement(By.name('btn-validate')).click();
+    await driver.wait(until.titleIs('added'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
