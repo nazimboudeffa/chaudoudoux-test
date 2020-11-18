f40e6a742a9dbe218d27a71117a9c335780939d3
Nazim Boudeffa <boudeffa@gmail.com>
2020-11-18 17:45:37 +0100

    Added Claude Jade as Candidate

--- candidate-claude-jade.js
+++ candidate-claude-jade.js
@@ -4,7 +4,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
   let driver = await new Builder().forBrowser('firefox').build();
   try {
     await driver.get('http://chaudoudoux.ml/candidates/add');
-    await driver.findElement(By.name('txtNom')).sendKeys('Mitchelli', Key.RETURN);
+    await driver.findElement(By.name('input-name')).sendKeys('Claude');
+    await driver.findElement(By.name('input-firstname')).sendKeys('Jade');
+    await driver.findElement(By.name('btn-validate')).click();
     await driver.wait(until.titleIs('added'), 1000);
   } finally {
     await driver.quit();
